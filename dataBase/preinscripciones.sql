839105d017e2a99b938cb32dce10e1d20c632286
alancorrea02 <alancorrea02@outlook.es>
2023-03-08 14:43:54 -0300

    m

--- dataBase/preinscripciones.sql
+++ dataBase/preinscripciones.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 02-03-2023 a las 18:58:29
+-- Tiempo de generación: 08-03-2023 a las 14:36:38
 -- Versión del servidor: 8.0.32-0ubuntu0.22.04.2
--- Versión de PHP: 8.1.2-1ubuntu2.10
+-- Versión de PHP: 8.1.2-1ubuntu2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,9 +30,16 @@ SET time_zone = "+00:00";
 CREATE TABLE `medicamentos` (
   `id` int NOT NULL,
   `nombre_comercial` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `medicamentos`
+--
+
+INSERT INTO `medicamentos` (`id`, `nombre_comercial`, `created_at`, `updated_at`) VALUES
+(1, 'Diazepam', '2023-03-04 14:09:06', NULL);
 
 -- --------------------------------------------------------
 
@@ -55,8 +62,9 @@ CREATE TABLE `personas` (
 --
 
 INSERT INTO `personas` (`id`, `nombre`, `apellido`, `dni`, `fecha_nacimiento`, `created_at`, `updated_at`) VALUES
-(1, 'alan', 'correa', '41877947', '1999-02-02', '2023-03-02 18:05:25', NULL),
-(2, 'dd', 'gorgi', '7777777', '1999-03-02', '2023-03-02 18:24:37', '2023-03-02 18:35:23');
+(3, 'gharu', 'vinland', '8949', '1999-02-04', '2023-03-03 01:22:29', NULL),
+(8, 'Solaire', 'De Astora', '45435665', '1999-03-02', '2023-03-06 19:00:59', NULL),
+(9, 'Patches', 'La hiena', '657658', '1999-03-02', '2023-03-06 19:01:22', NULL);
 
 -- --------------------------------------------------------
 
@@ -69,7 +77,7 @@ CREATE TABLE `persona_medicamento` (
   `observaciones` varchar(255) NOT NULL,
   `persona_id` int NOT NULL,
   `medicamento_id` int NOT NULL,
-  `created_at` timestamp NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -103,16 +111,22 @@ ALTER TABLE `persona_medicamento`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `medicamentos`
+--
+ALTER TABLE `medicamentos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de la tabla `personas`
 --
 ALTER TABLE `personas`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `persona_medicamento`
 --
 ALTER TABLE `persona_medicamento`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
@@ -122,7 +136,8 @@ ALTER TABLE `persona_medicamento`
 -- Filtros para la tabla `persona_medicamento`
 --
 ALTER TABLE `persona_medicamento`
-  ADD CONSTRAINT `medicamento_id` FOREIGN KEY (`medicamento_id`) REFERENCES `medicamentos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `medicamento_id` FOREIGN KEY (`medicamento_id`) REFERENCES `medicamentos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `persona_id` FOREIGN KEY (`persona_id`) REFERENCES `personas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
