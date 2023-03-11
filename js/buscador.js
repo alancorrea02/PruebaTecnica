7c57aad253bff535d388e3a0335d316a192c5630
alancorrea02 <alancorrea02@outlook.es>
2023-03-11 11:47:35 -0300

    sigo con detalles

--- js/buscador.js
+++ js/buscador.js
@@ -8,25 +8,23 @@ $(document).ready(function(){
             type: 'post',
             data: datar,
             success: function(response){
-                console.log(response);
                 let contenido = JSON.parse(response);
-            //console.log(contenido);
-            let template = '';
-            for (x in contenido){
-                template +=`
-                    <tr>
-                        <td>${contenido[x].id}</td>
-                        <td>${contenido[x].nombre}</td>
-                        <td>${contenido[x].apellido}</td>
-                        <td>${contenido[x].dni}</td>  
-                        <td>${contenido[x].nombre_comercial}</td>
-                        <td>${contenido[x].observaciones}</td>
-                        <td>${contenido[x].created_at}</td>    
-                        <td><button id="btnMod2" value=${contenido[x].id} type="button" style="margin: 10px;" class="btn btn-success"><img src="imagenes/pencil-square.svg"></button><button class="btn btn-danger" id="btnDel2" value=${contenido[x].id}><img src="imagenes/trash3-fill.svg"></button></td>
-                    </tr>
-                `;
-            }
-            $("#prescripciones").html(template);
+                let template = '';
+                for (x in contenido){
+                    template +=`
+                        <tr>
+                            <td>${contenido[x].id}</td>
+                            <td>${contenido[x].nombre}</td>
+                            <td>${contenido[x].apellido}</td>
+                            <td>${contenido[x].dni}</td>  
+                            <td>${contenido[x].nombre_comercial}</td>
+                            <td>${contenido[x].observaciones}</td>
+                            <td>${contenido[x].created_at}</td>    
+                            <td><button id="btnMod2" value=${contenido[x].id} type="button" style="margin: 10px;" class="btn btn-success"><img src="imagenes/pencil-square.svg"></button><button class="btn btn-danger" id="btnDel2" value=${contenido[x].id}><img src="imagenes/trash3-fill.svg"></button></td>
+                        </tr>
+                    `;
+                }
+                $("#prescripciones").html(template);
             }
 
         });
